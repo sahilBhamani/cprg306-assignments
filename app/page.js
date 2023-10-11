bbae2b0bca7e59c4f759a42194de0b81d60f2779
sahilBhamani <145397192+sahilBhamani@users.noreply.github.com>
2023-10-11 02:33:38 -0600

    week 3 assignment

--- app/page.js
+++ app/page.js
@@ -1,13 +1,13 @@
 import Link from "next/link";
-import StudentInfo from "./StudentInfo"; 
+import StudentInfo from '../app/StudentInfo'; // Use the correct relative path
 
 export default function cprg306() {
   return (
-    <div className="px-10 mx-10 my-20 text-xl">
+    <div className="px-10 mx-10 my-20 text-xl" style={{ backgroundColor: "white" }}>
       <h1 className="text-4xl">CPRG 306: Web Development 2 - Assignments</h1>
       <StudentInfo />
-      <Link href="/week2">week 2</Link><br /> 
-      <Link href="/week3">week 3</Link><br /> 
+      <Link href="/week2">week 2</Link><br />
+      <Link href="/week3">week 3</Link><br />
     </div>
   );
 }
